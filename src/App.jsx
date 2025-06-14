53825ed8ee1e5e9ee7b4227a0847b86c5139cb84
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2025-06-14 10:18:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Name from './components/name'
+
+function App() {
+
+  return (
+    <>
+      <Name/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Name.jsx
@@ -0,0 +1,12 @@
+function Name() {
+  return (
+    <div className="name">
+      <h1>John Doe</h1>
+      <h2>Web Developer</h2>
+      <h3>CSS and HTML</h3>
+    </div>
+  );
+}
+
+export default Name;
+// This component displays a name and title.
